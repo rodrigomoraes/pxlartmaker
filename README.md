265820ccab6e29dde335089c635b78ebfbb0ceef
rodrigo moraes <rodrigomoraes63@gmail.com>
2021-01-10 17:31:03 -0300

    atualização do readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-#pxlArt maker
+# Pixel Art Maker
+
+Pixel Art Maker feito com javascript puro, sem adição de bibliotecas externas.
+
+  - js comentado
+  - layout simples
+  - fins de estudo
+
 
--- pxlgen.js
+++ pxlgen.js
@@ -1,32 +1,25 @@
+/*
+Pixel Art maker versao 1.0
+Feito em VanillaJs por rodrigomoraes
+*/
+
 let c = document.getElementById('pColor');
 let cRes = document.getElementById('cRes');
-
-//Primeiro é criado um array vazio 
-//e definimos a altura e a largura do Canvas
 const pxArray = [];
-
-
-
-
 
 c.addEventListener("input", function(){
   cRes.style.backgroundColor = c.value;
 }, false);
 
-
 //Essa é a função pega os dados do form e desenha o canvas
-
 function draw(){
   let frm = document.querySelector("form.sizePicker");
   let wdt = document.querySelector("input.wd");
   let hgt = document.getElementById("hg");
 
-
   frm.addEventListener("submit", function(e){
     
     console.log(wd.value)
-
-
 
     let cWidth = wdt.value;
     let cHeight = hgt.value;
@@ -35,12 +28,8 @@ function draw(){
     renderPixel(cWidth, cHeight);
     colorPixel();
     e.preventDefault();
-
-    
   })
-
 }
-
 
 //Essa função calcula os pixels e injeta o array
 function calcPixel(cWidth, cHeight){
@@ -58,48 +47,30 @@ function renderPixel(cWidth, cHeight){
     for(let row=0; row < cHeight; row++){
       
       htm += '<tr>'
-      
         for(let col=0; col < cWidth; col++){
           const pxIndex = col + (cWidth * row);
           htm += '<td class="pixel">'
           //htm += pxIndex  
           htm += '</td>'
-        }
-        
+        } 
         htm += '</tr>'
       }
-      
       htm += '</table>'
-      
       document.querySelector('#pxCanvas').innerHTML = htm
-      
-      
     }
-    
 //Essa função faz adiciona cor aos pixels quando clicado
 function colorPixel(){
-  
   let x = document.querySelectorAll("td.pixel");
-  
   //document.querySelector("td.pixel").style.backgroundColor = "purple";
-
   Array.from(x).forEach(function(e){ 
-    
-    
     e.addEventListener("mousedown", function(){
       this.style.backgroundColor = c.value;
     });
-    
-    
   })
-  
   pxArray.forEach((item, index, array) => {
     console.log(pxArray[index]);
   });
 }
-
-
-
 draw();
 
 
